eebed39e4fed3a36c13aadc57649acc04e6aae45
Alan Burga <ahbb21@hotmail.com>
2025-04-11 01:39:45 -0500

    work application

--- backend/controllers/workController.js
+++ backend/controllers/workController.js
@@ -1,219 +1,265 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require("express-async-handler");
 
-const User = require('../models/userModel')
-const Work = require('../models/workModel')
-
+const User = require("../models/userModel");
+const Work = require("../models/workModel");
+const EmpProfile = require("../models/empProfileModel");
 
 // @desc    Get user works
 // @route   GET /api/works
 // @access  Private
-const getWorks = asyncHandler(async (req,res) => {
-    // Get works using id in the JWT
-    const user = await User.findById(req.user.id)
+const getWorks = asyncHandler(async (req, res) => {
+  // Get works using id in the JWT
+  const user = await User.findById(req.user.id);
 
-    if(!user){
-        res.status(401)
-        throw new Error('User not found')
-    }
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
 
-    const works = await Work.find({user: req.user.id})
+  const works = await Work.find({ user: req.user.id });
 
-    res.status(200).json(works)
-})
-
+  res.status(200).json(works);
+});
 
 // @desc    Get user works
 // @route   GET /api/works/:id
 // @access  Private
-const getWork = asyncHandler(async (req,res) => {
-    // Get works using id in the JWT
-    const user = await User.findById(req.user.id)
+const getWork = asyncHandler(async (req, res) => {
+  // Get works using id in the JWT
+  const user = await User.findById(req.user.id);
 
-    if(!user){
-        res.status(401)
-        throw new Error('User not found or not conected')
-    }
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found or not conected");
+  }
 
-    const work = await Work.findById(req.params.id)
+  const work = await Work.findById(req.params.id);
 
-    if(!work){
-        res.status(400)
-        throw new Error('Work not found')
-    }
+  if (!work) {
+    res.status(400);
+    throw new Error("Work not found");
+  }
 
-    if(work.user.toString() !== req.user.id){
-        res.status(401)
-        throw new Error('Not Authorizer')
-    }
+  if (work.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorizer");
+  }
 
-    res.status(200).json(work)
-})
+  // Traer solo algunos campos del perfil del candidato
+  const empProfile = await EmpProfile.findOne({
+    user: req.user.id,
+  }).select("companyName logo cover");
 
-
+  res.status(200).json({
+    ...work.toObject(),
+    companyProfile: empProfile || null, // Agregar el perfil de la empresa});
+  });
+});
 
 // @desc    create works
 // @route   POST /api/works
 // @access  Private
-const createWorks = asyncHandler(async (req,res) => {
-    
-    const {title, jobCategory, workTime, workWay, experience, qualification, workPay, country, city, workPlace, description, contactMail, workFunctions, workRequire, iDate, fDate, actTime, workStatus, active } = req.body
+const createWorks = asyncHandler(async (req, res) => {
+  const {
+    title,
+    jobCategory,
+    workTime,
+    workWay,
+    experience,
+    qualification,
+    workPay,
+    country,
+    city,
+    workPlace,
+    description,
+    contactMail,
+    workFunctions,
+    workRequire,
+    iDate,
+    fDate,
+    actTime,
+    workStatus,
+    active,
+  } = req.body;
 
-    if(!title || !description){
-        res.status(400)
-        throw new Error('Please add a title and description')
-    }
+  if (!title || !description) {
+    res.status(400);
+    throw new Error("Please add a title and description");
+  }
 
-    // Get works using id in the JWT
-    const user = await User.findById(req.user.id)
+  // Get works using id in the JWT
+  const user = await User.findById(req.user.id);
 
-    if(!user){
-        res.status(401)
-        throw new Error('Work not found')
-    }
+  if (!user) {
+    res.status(401);
+    throw new Error("Work not found");
+  }
 
-    const work = await Work.create({
-        title,
-        jobCategory,
-        workTime,
-        workWay,
-        experience,
-        qualification,
-        workPay,
-        country,
-        city,
-        workPlace,
-        description,
-        workFunctions,
-        workRequire,
-        contactMail,
-        iDate,
-        fDate,
-        actTime,
-        workStatus,
-        active,
-        location: workPlace + ", " + city + " - " + country,
-        user: req.user.id
-    })
+  const work = await Work.create({
+    title,
+    jobCategory,
+    workTime,
+    workWay,
+    experience,
+    qualification,
+    workPay,
+    country,
+    city,
+    workPlace,
+    description,
+    workFunctions,
+    workRequire,
+    contactMail,
+    iDate,
+    fDate,
+    actTime,
+    workStatus,
+    active,
+    location: workPlace + ", " + city + " - " + country,
+    user: req.user.id,
+  });
 
-    res.status(201).json(work)
-    console.log(work)
-})
+  res.status(201).json(work);
+  console.log(work);
+});
 
 // @desc    Delete user works
 // @route   DELETE /api/works/:id
 // @access  Private
-const deleteWork = asyncHandler(async (req,res) => {
-    // Get works using id in the JWT
-    const user = await User.findById(req.user.id)
+const deleteWork = asyncHandler(async (req, res) => {
+  // Get works using id in the JWT
+  const user = await User.findById(req.user.id);
 
-    if(!user){
-        res.status(401)
-        throw new Error('User not found or not conected')
-    }
-    console.log("controller: ", req.params.id)
-    const work = await Work.findById(req.params.id)
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found or not conected");
+  }
+  console.log("controller: ", req.params.id);
+  const work = await Work.findById(req.params.id);
 
-    if(!work){
-        res.status(400)
-        throw new Error('Work not found')
-    }
+  if (!work) {
+    res.status(400);
+    throw new Error("Work not found");
+  }
 
-    if(work.user.toString() !== req.user.id){
-        res.status(401)
-        throw new Error('Not Authorizer')
-    }
+  if (work.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorizer");
+  }
 
-    await Work.findByIdAndDelete({ _id: req.params.id });
+  await Work.findByIdAndDelete({ _id: req.params.id });
 
-    res.status(200).json({success: true})
-})
+  res.status(200).json({ success: true });
+});
 
 // @desc    Update user works
 // @route   PUT /api/works/:id
 // @access  Private
-const updateWork = asyncHandler(async (req,res) => {
-    // Get works using id in the JWT
-    const user = await User.findById(req.user.id)
+const updateWork = asyncHandler(async (req, res) => {
+  // Get works using id in the JWT
+  const user = await User.findById(req.user.id);
 
-    if(!user){
-        res.status(401)
-        throw new Error('User not found or not conected')
-    }
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found or not conected");
+  }
 
-    const work = await Work.findById(req.params.id)
+  const work = await Work.findById(req.params.id);
 
-    if(!work){
-        res.status(400)
-        throw new Error('Work not found')
-    }
+  if (!work) {
+    res.status(400);
+    throw new Error("Work not found");
+  }
 
-    if(work.user.toString() !== req.user.id){
-        res.status(401)
-        throw new Error('Not Authorizer')
-    }
+  if (work.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorizer");
+  }
 
-    const {title, jobCategory, workTime, workWay, experience, qualification, workPay, country, city, workPlace, description, workFunctions, workRequire, contactMail, iDate, fDate, actTime, workStatus, active } = req.body
+  const {
+    title,
+    jobCategory,
+    workTime,
+    workWay,
+    experience,
+    qualification,
+    workPay,
+    country,
+    city,
+    workPlace,
+    description,
+    workFunctions,
+    workRequire,
+    contactMail,
+    iDate,
+    fDate,
+    actTime,
+    workStatus,
+    active,
+  } = req.body;
 
-    const updatedWork = await Work.findByIdAndUpdate(req.params.id, {
-        title,
-        jobCategory,
-        workTime,
-        workWay,
-        experience,
-        qualification,
-        workPay,
-        country,
-        city,
-        workPlace,
-        description,
-        workFunctions,
-        workRequire,
-        contactMail,
-        iDate,
-        fDate,
-        actTime,
-        workStatus,
-        active,
-        location: workPlace + ", " + city + " - " + country,
-        user: req.user.id
-    }, {new:true})
+  const updatedWork = await Work.findByIdAndUpdate(
+    req.params.id,
+    {
+      title,
+      jobCategory,
+      workTime,
+      workWay,
+      experience,
+      qualification,
+      workPay,
+      country,
+      city,
+      workPlace,
+      description,
+      workFunctions,
+      workRequire,
+      contactMail,
+      iDate,
+      fDate,
+      actTime,
+      workStatus,
+      active,
+      location: workPlace + ", " + city + " - " + country,
+      user: req.user.id,
+    },
+    { new: true }
+  );
 
-    res.status(200).json(updatedWork)
-})
+  res.status(200).json(updatedWork);
+});
 
 // @desc    Get All works
 // @route   GET /allworks/
 // @access  Public
 
-const getAllWorks = asyncHandler(async (req,res) => {
-    const work = await Work.find()
+const getAllWorks = asyncHandler(async (req, res) => {
+  const work = await Work.find();
 
-    //filtrar solo los elementos necesarios para el JobList
-    
-    res.status(200).json(work)
-})
+  //filtrar solo los elementos necesarios para el JobList
+
+  res.status(200).json(work);
+});
 
 // @desc    Get user works
 // @route   GET /allworks/:id
 // @access  Public
-const getPublicWork = asyncHandler(async (req,res) => {
-    
-    const work = await Work.findById(req.params.id)
+const getPublicWork = asyncHandler(async (req, res) => {
+  const work = await Work.findById(req.params.id);
 
-    if(!work){
-        res.status(400)
-        throw new Error('Work not found')
-    }
-    res.status(200).json(work)
-})
-
+  if (!work) {
+    res.status(400);
+    throw new Error("Work not found");
+  }
+  res.status(200).json(work);
+});
 
 module.exports = {
-    getWorks,
-    getWork,
-    createWorks,
-    updateWork,
-    deleteWork,
-    getAllWorks,
-    getPublicWork
-}
+  getWorks,
+  getWork,
+  createWorks,
+  updateWork,
+  deleteWork,
+  getAllWorks,
+  getPublicWork,
+};
