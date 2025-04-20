3d1e99029c6d0cb34e03e1a6b373026e210f2435
Alan Burga <ahbb21@hotmail.com>
2025-04-20 12:27:01 -0500

    work list

--- backend/routes/workRoutes.js
+++ backend/routes/workRoutes.js
@@ -15,15 +15,13 @@ const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getWorks).post(protect, createWorks);
 
+router.route("/allpaginated").get(getAllWorksPaginated);
+router.route("/all").get(getAllWorks);
+router.route("/all/:id").get(getPublicWork);
 router
   .route("/:id")
   .get(protect, getWork)
   .put(protect, updateWork)
   .delete(protect, deleteWork);
 router.route("/work/:id").get(protect, getPublicWork);
-
-router.route("/allpaginated").get(getAllWorksPaginated);
-router.route("/all").get(getAllWorks);
-router.route("/all/:id").get(getPublicWork);
-
 module.exports = router;
